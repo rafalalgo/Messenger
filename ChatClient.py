6ba273487f0e77605c7ff4fcaa2bb28b7d2293cc
Rafał Byczek <rafal.algo@gmail.com>
2016-12-10 16:55:43 +0100

    poprawki

--- ChatClient.py
+++ ChatClient.py
@@ -4,195 +4,177 @@ import socket
 import sys
 import Tkinter as tk
 
-global name, host, port, buforSize, endClient, users, temp, lock, client_socket
-global should, myapp, ok, message
-global sendThread, recvThread
 
-host = 'localhost'
-port = 44000
-buforSize = 2048
-endClient = False
-users = []
-temp = []
-should = False
-myapp = None
-message = ""
-
-
-def send_data(client):
-    global endClient, myapp, name, should
-    while True:
-        print(1)
-        if endClient:
-            return
-        if should:
-            data = myapp.users.curselection()
-            if len(data) == 0:
-                data = myapp.users.get(0) + "#"
-            else:
-                data = myapp.users.get(data[0]) + "#"
-            res = myapp.textbox.get("1.0", tk.END)
-            print("\t\t" + res)
-            if len(res) <= 2:
-                tkMessageBox.showerror("Error", "Nie podales wiadomosci do wyslania.")
-                myapp.textbox.delete("1.0", tk.END)
-                should = 0
-                continue
-            data += res
-            myapp.textbox.delete("1.0", tk.END)
-            client.send(data)
-            if send_data == "LOGOUT":
-                lock.acquire()
-                endClient = True
-                client.close()
-                lock.release()
-                break
-            should = False
-
-
-def recv_data(client):
-    global endClient
-    while True:
-        print(2)
-        if endClient:
-            return
-        try:
-            data = client.recv(buforSize)
-        except:
-            lock.acquire()
-            endClient = True
-            lock.release()
-            break
-        if not data:
-            lock.acquire()
-            endClient = True
-            lock.release()
-            break
-        if data[0:4] == "LIST":
-            response = data[9:]
-            global users, myapp
-            lock.acquire()
-            users = response.split('#')
-            users = filter(lambda a: a != '#' and a != '', users)
-            myapp.update_list()
-            lock.release()
-        elif data[0:3] == "MSG":
-            global temp, message
-            lock.acquire()
-            response = data[3:]
-            temp = response.split('#')
-            temp = filter(lambda a: a != '#' and a != '', temp)
-            message = temp[0] + " ==> " + temp[1] + ":\n"
-            message += temp[2]
-            lock.release()
-
-
-def start_client():
-    global client_socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    client_socket.connect((host, port))
-
-    global name
-    name = str(raw_input("Name: "))
-    client_socket.send(name)
-    response = client_socket.recv(buforSize)
-    if response == "0":
-        print("ERROR. TRY AGAIN.")
-        return
-    if response == "1":
-        global lock
-        lock = Lock()
-        global myapp, sendThread, recvThread
-        root = tk.Tk()
-        myapp = MyApp(root)
-        recvThread = Thread(target=recv_data, args=(client_socket,))
-        recvThread.start()
-        sendThread = Thread(target=send_data, args=(client_socket,))
-        sendThread.start()
-        root.mainloop()
-
-
-class MyApp:
-    def __init__(self, root):
+class MyWindow:
+    def __init__(self, root, name):
+        self.name = name
         self.root = root
-        self.root.title("Chat")
+        self.root.title("Chat: " + self.name)
         self.root.minsize(600, 400)
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=2)
         self.root.columnconfigure(1, weight=1)
 
-        self.ChatF = tk.Frame(self.root)
-        self.ChatF.grid(column=0, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
-        self.ChatF.rowconfigure(0, weight=3)
-        self.ChatF.rowconfigure(1, weight=2)
-        self.ChatF.rowconfigure(2, weight=1)
-        self.ChatF.columnconfigure(0, weight=1)
+        self.chatFrame = tk.Frame(self.root)
+        self.chatFrame.grid(column=0, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.chatFrame.rowconfigure(0, weight=3)
+        self.chatFrame.rowconfigure(1, weight=2)
+        self.chatFrame.rowconfigure(2, weight=1)
+        self.chatFrame.columnconfigure(0, weight=1)
 
-        self.UsersF = tk.Frame(self.root)
-        self.UsersF.grid(column=1, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
-        self.UsersF.rowconfigure(0, weight=5)
-        self.UsersF.rowconfigure(1, weight=1)
-        self.UsersF.columnconfigure(0, weight=1)
+        self.userListFrame = tk.Frame(self.root)
+        self.userListFrame.grid(column=1, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.userListFrame.rowconfigure(0, weight=5)
+        self.userListFrame.rowconfigure(1, weight=1)
+        self.userListFrame.columnconfigure(0, weight=1)
 
-        self.sendbutton = tk.Button(self.ChatF, text="Send Message")
-        self.sendbutton.bind("<Button-1>", self.sending)
-        self.sendbutton.grid(column=0, row=2, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.sendButton = tk.Button(self.chatFrame, text="Send Message")
+        self.sendButton.grid(column=0, row=2, sticky=tk.N + tk.S + tk.W + tk.E)
 
-        self.exitbutton = tk.Button(self.UsersF, text="Exit")
-        self.exitbutton.bind("<Button-1>", self.exit)
-        self.exitbutton.grid(column=0, row=1, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.exitButton = tk.Button(self.userListFrame, text="Exit")
+        self.exitButton.grid(column=0, row=1, sticky=tk.N + tk.S + tk.W + tk.E)
 
-        self.textbox = tk.Text(self.ChatF, height=0, width=0)
-        self.textbox.grid(column=0, row=1, sticky=tk.N + tk.S + tk.W + tk.E)
-        self.textbox.grid(column=0, row=1, sticky=tk.N + tk.S + tk.W + tk.E)
-        self.textbox.bind("<Return>", self.sending)
+        self.textBox = tk.Text(self.chatFrame, height=0, width=0)
+        self.textBox.grid(column=0, row=1, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.textBox.grid(column=0, row=1, sticky=tk.N + tk.S + tk.W + tk.E)
 
-        self.chatbox = tk.Text(self.ChatF, height=0, width=0)
-        self.chatbox.config(state=tk.DISABLED)
-        self.chatbox.grid(column=0, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.chatBox = tk.Text(self.chatFrame, height=0, width=0)
+        self.chatBox.config(state=tk.DISABLED)
+        self.chatBox.grid(column=0, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
 
-        self.users = tk.Listbox(self.UsersF, height=0, width=0)
-        self.users.grid(column=0, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
-        self.num_users = 0
+        self.userList = tk.Listbox(self.userListFrame, height=0, width=0)
+        self.userList.grid(column=0, row=0, sticky=tk.N + tk.S + tk.W + tk.E)
+
+
+class MyApp:
+    def __init__(self, root, name):
+        self.myWindow = MyWindow(root, name)
+        self.myWindow.sendButton.bind("<Button-1>", self.sending)
+        self.myWindow.exitButton.bind("<Button-1>", self.exit)
+        self.myWindow.textBox.bind("<Return>", self.sending)
+        self.name = name
+        self.num_userList = 0
+        self.lock = Lock()
+        self.should = False
+        self.endClient = False
+        self.nameUsers = []
+        self.message = ""
         self.update_list()
         Thread(target=self.run, args=[]).start()
 
     def sending(self, event):
-        global should
-        should = 1
+        self.should = True
 
     def update_list(self):
-        self.users.delete(0, self.users.size())
-        global users
-        self.num_users = 0
-        for item in users:
-            self.users.insert(self.num_users, item)
-            self.num_users += 1
+        self.myWindow.userList.delete(0, self.myWindow.userList.size())
+        self.num_userList = 0
+        for item in self.nameUsers:
+            self.myWindow.userList.insert(self.num_userList, item)
+            self.num_userList += 1
 
     def exit(self, event):
-        global should, sendThread, recvThread, client_socket
-        should = True
-        global endClient
-        endClient = True
+        global client_socket
+        self.should = True
+        self.endClient = True
         client_socket.send("LOGOUT")
         client_socket.close()
-        self.root.destroy()
+        self.myWindow.root.destroy()
         sys.exit()
 
     def run(self):
-        global message, endClient
         while 1:
             print(3)
-            if endClient:
+            if self.endClient:
                 break
-            if len(message) > 1:
-                lock.acquire()
-                self.chatbox.config(state="normal")
-                self.chatbox.insert(tk.INSERT, message)
-                self.chatbox.config(state=tk.DISABLED)
-                message = ""
-                lock.release()
+            if len(self.message) > 1:
+                self.lock.acquire()
+                self.myWindow.chatBox.config(state="normal")
+                self.myWindow.chatBox.insert(tk.INSERT, self.message)
+                self.myWindow.chatBox.config(state=tk.DISABLED)
+                self.message = ""
+                self.lock.release()
+
+    def send_data(self, client):
+        while True:
+            print(1)
+            if self.endClient:
+                return
+            if self.should:
+                data = self.myWindow.userList.curselection()
+                if len(data) == 0:
+                    data = self.myWindow.userList.get(0) + "#"
+                else:
+                    data = self.myWindow.userList.get(data[0]) + "#"
+                res = self.myWindow.textBox.get("1.0", tk.END)
+                print("\t\t" + res)
+                if len(res) <= 1:
+                    tkMessageBox.showerror("Error", "Nie podales wiadomosci do wyslania.")
+                    self.textBox.delete("1.0", tk.END)
+                    self.should = 0
+                    continue
+                data += res
+                self.myWindow.textBox.delete("1.0", tk.END)
+                client.send(data)
+                if self.send_data == "LOGOUT":
+                    self.lock.acquire()
+                    self.endClient = True
+                    client.close()
+                    self.lock.release()
+                    break
+                self.should = False
+
+    def recv_data(self, client):
+        while True:
+            print(2)
+            if self.endClient:
+                return
+            try:
+                data = client.recv(buforSize)
+            except:
+                self.lock.acquire()
+                self.endClient = True
+                self.lock.release()
+                break
+            if not data:
+                self.lock.acquire()
+                self.endClient = True
+                self.lock.release()
+                break
+            if data[0:4] == "LIST":
+                data = data[9:]
+                self.lock.acquire()
+                self.nameUsers = data.split('#')
+                self.nameUsers = filter(lambda a: a != '#' and a != '', self.nameUsers)
+                self.update_list()
+                self.lock.release()
+            elif data[0:3] == "MSG":
+                self.lock.acquire()
+                data = data[3:]
+                temp = data.split('#')
+                temp = filter(lambda a: a != '#' and a != '', temp)
+                self.message = temp[0] + " ==> " + temp[1] + ":\n"
+                self.message += temp[2]
+                self.lock.release()
 
 
-start_client()
+host = 'localhost'
+port = 44000
+buforSize = 2048
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket.connect((host, port))
+
+nameN = str(raw_input("Name: "))
+client_socket.send(nameN)
+response = client_socket.recv(buforSize)
+
+if response == "0":
+    print("ERROR. TRY AGAIN.")
+if response == "1":
+    rootR = tk.Tk()
+    myapp = MyApp(rootR, nameN)
+    recv_thread = Thread(target=myapp.recv_data, args=(client_socket,))
+    recv_thread.start()
+    send_thread = Thread(target=myapp.send_data, args=(client_socket,))
+    send_thread.start()
+    rootR.mainloop()
